bf83b212145fa6e1e3973b94f82778a743942611
William Cooper <william.cooper91231@gmail.com>
2022-01-16 22:35:27 +1300

    added button

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom"
 
 function App() {
+  const [isImageActive, setIsImageActive] = useState(false);
+  function clickEventHandler() {
+    setIsImageActive(true);
+  }
   return (
     <div>
-      <img src={process.env.PUBLIC_URL + "ciaran-head.png"} alt="Ciaran"/>
+      {!isImageActive && <button onClick={clickEventHandler}>Click for Ciaran</button> }
+      {isImageActive && <img src={process.env.PUBLIC_URL + "ciaran-head.png"} alt="Ciaran " />}
       <Outlet/>
     </div>
   );
-}
+};
+
 
 export default App;
