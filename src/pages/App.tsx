c45e5fc2d49cfd5db0c1c3d28ecf3596872337d1
William Cooper <william.cooper91231@gmail.com>
2022-01-17 22:54:16 +1300

    added further buttons

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,42 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom"
+import { Button, Pane, Text, SearchInput } from "evergreen-ui"
 
 function App() {
   const [isImageActive, setIsImageActive] = useState(false);
-  function clickEventHandler() {
+  const [photoName, setPhotoName] = useState("")
+  function clickEventHandler( person: string ) {
+    setPhotoName(person)
     setIsImageActive(true);
   }
   return (
-    <div>
-      {!isImageActive && <button onClick={clickEventHandler}>Click for Ciaran</button> }
-      {isImageActive && <img src={process.env.PUBLIC_URL + "ciaran-head.png"} alt="Ciaran " />}
-      <Outlet/>
-    </div>
+    <Pane>
+      <Pane height={50} backgroundColor="#eeeee4" clearfix>
+        <Pane width="33.33%" float="left" display="flex" alignItems="center" justifyContent="center">
+          <Text margin={8} size={500}>
+            thewillygoat.fun</Text>
+        </Pane>
+        <Pane width="33.33%" float="left" display="flex" alignItems="center" justifyContent="center">
+          <SearchInput margin={8}>
+          </SearchInput>
+        </Pane>
+        <Pane width="33.33%" float="left" display="flex" alignItems="right">
+        </Pane>
+      </Pane>
+      <Pane margin={10} alignItems="center" width="100%" justifyContent="center">
+        <Pane width="50%" height="50%" display="flex" float="left" justifyContent="center" flexDirection="column">
+          <Button margin={20} onClick={() => clickEventHandler("ciaran.png")}>Click for Ciaran</Button>
+          <Button margin={20} onClick={() => clickEventHandler("arvin.png")}>Click for Arvin</Button>
+          <Button margin={20} onClick={() => clickEventHandler("mady.png")}>Click for Mady</Button>
+          <Button margin={20} onClick={() => clickEventHandler("grace.png")}>Click for Grace</Button>
+        </Pane>
+        <Pane width="50%" display="flex" float="left" justifyContent="center">
+          {isImageActive && <img src={process.env.PUBLIC_URL + photoName} alt="Ciaran " />}
+        </Pane>
+        
+      </Pane>
+      <Outlet />
+    </Pane>
   );
 };
 
