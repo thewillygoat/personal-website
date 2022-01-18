cf8c452e4848cbecbc9309cb3d65b35b2c51e1bb
William Cooper <william.cooper91231@gmail.com>
2022-01-18 15:29:14 +1300

    integrated with terraform cloud

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,14 @@
 
 
+terraform {
+  cloud {
+    organization = "thewillygoat-org"
+    workspaces {
+        name = "personal-website"
+    }
+  }
+}
+
 module "elastic_beanstalk" {
     source = "./../modules/elastic_beanstalk"
     ebs_name = "personal-website"
