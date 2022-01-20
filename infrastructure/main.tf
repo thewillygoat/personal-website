758dc100ee07843e9eaa23f48afedbe14d9acf53
William Cooper <william.cooper91231@gmail.com>
2022-01-20 18:22:43 +1300

    moved website into web

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,15 +13,3 @@ module "elastic_beanstalk" {
     source = "./../modules/elastic_beanstalk"
     ebs_name = "personal-website"
 }
-
-# resource "aws_elastic_beanstalk_application" "personal-website" {
-#     name = var.ebs_name
-#     description = "my personal website host"
-
-# }
-
-# resource "aws_elastic_beanstalk_environment" "personal-website-env" {
-#     name = "${var.ebs_name}-env"
-#     application = aws_elastic_beanstalk_application.personal_website.name
-#     solution_stack_name = "64bit Amazon Linux 2 v5.4.9 running Node.js 14"
-# }
